cfb395867bb2ca8296e23d602a953289ac11077f
Ivena <huangzhy77@outlook.com>
2025-04-01 00:50:25 -0400

    tmp rustlings till hashMap

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -3,10 +3,12 @@
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
+    Quit,
+    Echo(String),
+    Move { x: i32, y: i32 },
+    ChangeColor(u8, u8, u8),
     // TODO: define the different variants used below
 }
 
